c029446847b74c9c88a8929d36170fbbdc7444e5
Skvortsovvg <vovanches@i.ua>
2022-03-24 16:24:55 +0200

    intro

--- game.exe.rb
+++ game.exe.rb
@@ -1,1 +1,20 @@
- 
+
+require 'io/console'
+
+def print_shift(text)
+  print "\t\t\t\t\t\t" + text
+end
+
+system 'cls'
+
+input   = File.open("logo.txt",  "r")
+
+while (line = input.gets) do
+ puts line
+end
+
+print_shift "Нажмите любую клавиу для начала игры..."
+STDIN.getch
+print "\r"
+print_shift "======================================="
+STDIN.getch
